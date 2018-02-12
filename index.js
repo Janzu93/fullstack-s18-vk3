f5c3c23794bad48e945100aa95b242a3499787fd
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2018-02-12 19:26:19 +0200

    3.4

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ app.get('/info', (req, res) => {
     res.end(`<p>puhelinluettelossa on ${count} henkilön tiedot</p><p>${time}</p>`)
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
